333e857db8f19e25df639c17047bea038f7b59c9
xgodz <35343080+xgodz@users.noreply.github.com>
2021-02-07 12:41:19 +0530

    Hello WOrld Multiplayer Edition

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{ 
+   void HandleMovement()
+   {
+        if(isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal *0.1f,moveVertical *0.1f,0);
+            transform.position = transform.position + movement;
+        }
+   }
+    void Update()
+    {
+        HandleMovement();
+    }
+}
